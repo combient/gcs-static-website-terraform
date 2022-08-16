f6cd87d81e1e4e69772cb75f3473d3d240933f38
torstenek <torstenek@gmail.com>
2022-08-16 15:42:44 +0200

    Removed defaults
    Removed silly defaults for hosted_zone_name and hosted_zone_dns_name that would just be an irritant if they got deployed. Now they are required.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
 variable "project_id" {}
 variable "region" {}
 variable "hosted_zone_name" {
-  default = "radar-zone"
 }
 variable "hosted_zone_dns_name" {
   # Note that this has to be an absolute domain name (i.e. a trailing period. see RFC 1034)
-  default = "example.com."
+  # Example "example.com."
 }
 variable "websites" {
   type = map(object({
